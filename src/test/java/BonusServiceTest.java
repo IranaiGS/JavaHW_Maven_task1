bb181f98da38b1c296a03139dea9f2ee7ecd1d2b
Irina Gosteva <IrenaiAdler@yandex.ru>
2023-06-25 23:48:14 +0300

    correct test4

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -55,9 +55,9 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1_000_000;
+        long amount = 1000;
         boolean registered = false;
-        long expected = 500;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
